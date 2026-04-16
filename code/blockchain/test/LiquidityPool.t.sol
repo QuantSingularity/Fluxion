e7b06d13db1b53ab9fa7416343d9482317655eb5
Abrar Ahmed <abrarahmedny@gmail.com>
2026-04-16 14:04:34 +0500

    Implement updates for better performance

--- code/blockchain/test/LiquidityPool.t.sol
+++ code/blockchain/test/LiquidityPool.t.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.19;
 
-import 'forge-std/Test.sol';
-import '../contracts/LiquidityPoolManager.sol';
+import "forge-std/Test.sol";
+import "../contracts/LiquidityPoolManager.sol";
 
 contract LiquidityPoolTest is Test {
-  LiquidityPoolManager pool;
-  address[] assets;
-  uint256[] weights;
+    LiquidityPoolManager pool;
+    address[] assets;
+    uint256[] weights;
 
-  function setUp() public {
-    pool = new LiquidityPoolManager();
-    assets = [address(1), address(2)];
-    weights = [5e17, 5e17]; // 50/50 weights
-  }
+    function setUp() public {
+        pool = new LiquidityPoolManager();
+        assets = [address(1), address(2)];
+        weights = [5e17, 5e17]; // 50/50 weights
+    }
 
-  function testPoolCreation() public {
-    pool.createPool(assets, weights, 0.003e18, 100);
-    bytes32 poolId = keccak256(abi.encode(assets, weights));
-    assertEq(pool.pools(poolId).fee, 0.003e18);
-  }
+    function testPoolCreation() public {
+        pool.createPool(assets, weights, 0.003e18, 100);
+        bytes32 poolId = keccak256(abi.encode(assets, weights));
+        assertEq(pool.pools(poolId).fee, 0.003e18);
+    }
 }
